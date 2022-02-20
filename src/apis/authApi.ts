4580d842a2ae57da5d1b60393bbf70cbb67ec3ab
Jiwon Kim <lost731@naver.com>
2022-02-20 21:27:29 +0900

    Add: AuthProvider

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -1,12 +1,36 @@
 import axios from 'axios'
+import { useMutation } from 'react-query'
 import { authUrl } from '../config/config'
 
 axios.defaults.baseURL = authUrl
+
+export interface TokenSet {
+  accessToken?: string
+  refreshToken?: string
+  tokenExpires: string
+}
 
 export function loadMyInfoAPI() {
   return axios.get('/user')
 }
 
-export function logInAPI(data: any): object {
-  return axios.post('/login', data)
+function logInAPI(no: string, password: string): any {
+  return axios.post('/login', {
+    no,
+    password,
+  })
+}
+
+export const loginRequest = async (no: string, password: string) => {
+  const { accessToken, refreshToken, tokenExpires }: TokenSet = await logInAPI(no, password)
+  return { accessToken, refreshToken, tokenExpires }
+}
+
+export function refreshTokenApi(): any {
+  return axios.get('/refreshToken')
+}
+
+export const refreshRequest = async () => {
+  const { accessToken, refreshToken, tokenExpires }: TokenSet = await refreshTokenApi()
+  return { accessToken, refreshToken, tokenExpires }
 }
---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,49 @@
+import axios, { AxiosRequestConfig } from 'axios'
+import React, { useRef, useState, useEffect, createContext } from 'react'
+import { useMutation } from 'react-query'
+import { loginRequest, refreshRequest } from '../apis/authApi'
+import { configure } from 'axios-hooks'
+
+function AuthProvider(props: any) {
+  const AuthContext = createContext(false)
+  const accessTokenRef = useRef<string>()
+  const [tokenExpires, setTokenExpires] = useState<string>('')
+
+  const logInQuery = useMutation(loginRequest, {
+    onSuccess: (data: any) => {
+      accessTokenRef.current = data.accessToken
+      setTokenExpires(data.tokenExpires)
+    },
+  })
+
+  const refreshQuery = useMutation(refreshRequest, {
+    onSuccess: (data) => {
+      accessTokenRef.current = data.accessToken
+      setTokenExpires(data.tokenExpires)
+    },
+  })
+  useEffect(() => {
+    // add authorization token to each request
+    axios.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig => {
+      //   config.headers.authorization = `Bearer ${accessTokenRef.current}`
+      // this is important to include the cookies when we are sending the requests to the backend.
+      config.withCredentials = true
+      return config
+    })
+
+    axios.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        return Promise.reject(error)
+      }
+    )
+
+    // configure axios-hooks to use this instance of axios
+    configure({ axios })
+  }, [])
+  const isSuccess = logInQuery.isSuccess || refreshQuery.isSuccess
+  const isAuthenticated = isSuccess && !!accessTokenRef.current
+  return <AuthContext.Provider value={...props}></AuthContext.Provider>
+}
+
+export default AuthProvider
